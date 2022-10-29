63c1704355d2db801c8187e8c252ca13f5325c94
Tyler Mueller <muelltyl@gmail.com>
2022-10-29 14:25:41 -0500

    adds web server

--- app.ts
+++ app.ts
@@ -2,4 +2,8 @@ import { Server } from './src/Router.ts';
 
 const server = new Server();
 
+server.router.get(/\/*/, req => {  
+    req.respondWith(new Response(Deno.inspect(new URL(req.request.url))))
+})
+
 server.listen();
--- src/Router.ts
+++ src/Router.ts
@@ -1,6 +1,37 @@
+type pathname = string | RegExp;
 
-export default class Router {
-
+class Router {
+    private GET: Map<string, (req: Deno.RequestEvent) => void> = new Map;
+    private GETREGEX: Map<RegExp, (req: Deno.RequestEvent) => void> = new Map;
+    private POST: Map<pathname, (req: Deno.RequestEvent) => void> = new Map;
+    private POSTREGEX: Map<RegExp, (req: Deno.RequestEvent) => void> = new Map;
+    get(pathname: pathname, req: (req: Deno.RequestEvent) => void) {
+        if(pathname instanceof RegExp) return this.GETREGEX.set(pathname, req);
+        if(typeof pathname === 'string') return this.GET.set(pathname, req);
+    }
+    post(pathname: pathname, req: (req: Deno.RequestEvent) => void) {
+        if(pathname instanceof RegExp) return this.POSTREGEX.set(pathname, req);
+        if(typeof pathname === 'string') return this.POST.set(pathname, req);
+    }
+    handle(req: Deno.RequestEvent) {
+        const url = new URL(req.request.url);
+        
+        if(req.request.method === 'GET') {
+            for(const key of this.GET.keys())
+                if(url.pathname === key) return this.GET.get(key)!(req);
+            for(const key of this.GETREGEX.keys())
+                if(key.test(url.pathname)) return this.GETREGEX.get(key)!(req);
+        }
+        if(req.request.method === 'POST') {
+            for(const key of this.POST.keys())
+                if(url.pathname === key) return this.POST.get(key)!(req);
+            for(const key of this.POSTREGEX.keys())
+                if(key.test(url.pathname)) return this.POSTREGEX.get(key)!(req);
+        }
+        req.respondWith(new Response('', {
+            status: 404
+        }));
+    }
 }
 
 export class Server {
@@ -10,12 +41,12 @@ export class Server {
         reusePort: true
     }
     TcpListener: Deno.Listener;
-    Routes = {
-        GET: [],
-        POST: []
-    }
-    constructor(options?: Partial<Deno.TcpListenOptions>) {
+    router: Router;
+    constructor(options?: Partial<Deno.TcpListenOptions & Partial<{
+        router: Router
+    }>>) {
         this.TcpListener = Server.createListener(options);
+        this.router = options?.router ? options.router : new Router();
     }
     static createListener(options?: Partial<Deno.TcpListenOptions>): Deno.Listener {
         return Deno.listen({
@@ -28,7 +59,7 @@ export class Server {
             for await (const request of Deno.serveHttp(conn))
                 this.handle(request);
     }
-    async handle(req: Deno.RequestEvent) {
-        console.log(req.request)
+    handle(req: Deno.RequestEvent) {
+        return this.router.handle(req);
     }
 }
